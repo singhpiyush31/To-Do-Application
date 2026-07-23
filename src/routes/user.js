85f52b8c83f258421e35c20653cbf48255892318
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-23 23:23:33 +0530

    Update user API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const userAuth = require('../middlewares/auth');
-const user = require('../models/user');
+const User = require('../models/user');
 
 const userRouter = express.Router();
 
@@ -13,6 +13,22 @@ userRouter.get("/profile", userAuth, (req,res) => {
         res.status(400).send("ERROR: " + err.message);
     }
     
+})
+
+userRouter.patch("/update" , userAuth, async (req,res) => {
+    try {
+        const { name } = req.body;
+        const { _id } = req.user;
+
+        const updatedUser = await User.findByIdAndUpdate(_id,
+            { name: name },
+            { returnDocument: 'after' }
+        ).select("-password -__v")
+
+        res.status(200).json({ user: updatedUser, message: "User updated successfully!" })
+    } catch (err) {
+        res.status(500).json({ error: err.message, message: "Internal Server Error" });
+    }
 })
 
 module.exports = userRouter;
